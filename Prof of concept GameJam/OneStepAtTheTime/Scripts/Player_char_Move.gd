8614ff6242b3174a790efadd148f1446aec949cf
Wilisimple4u <112163287+Wilisimple4u@users.noreply.github.com>
2025-07-19 21:22:16 +0200

    UpdatePlayerPlatform

--- Prof of concept GameJam/OneStepAtTheTime/Scripts/Player_char_Move.gd
+++ Prof of concept GameJam/OneStepAtTheTime/Scripts/Player_char_Move.gd
@@ -12,6 +12,8 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+		
+
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
