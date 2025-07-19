2688870a46fe9a1e19aeb10fe680e681fb947bad
Wilisimple4u <112163287+Wilisimple4u@users.noreply.github.com>
2025-07-19 21:39:45 +0200

    YouCanMakePlatforms!!!!

--- Prof of concept GameJam/OneStepAtTheTime/Scripts/Platform_spawner.gd
+++ Prof of concept GameJam/OneStepAtTheTime/Scripts/Platform_spawner.gd
@@ -3,7 +3,7 @@ extends StaticBody2D
 #Ready's the player platform for use and referance inside the script.
 @onready var PlacePlatform = preload("res://OneStepAtTheTime/ObjectScenes/PlayerPlatform.tscn")
 
-func process():
+func _physics_process(delta: float):
 	if Input.is_action_just_pressed("ui_down"):
 		var PlatformPlaced = PlacePlatform.instantiate()
 		add_child(PlatformPlaced)
--- Prof of concept GameJam/OneStepAtTheTime/Scripts/Player_char_Move.gd
+++ Prof of concept GameJam/OneStepAtTheTime/Scripts/Player_char_Move.gd
@@ -1,18 +1,20 @@
 extends CharacterBody2D
 
 
+
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 # IF the character hits a triangular surface,
 # which sends them just within range of being on top of another platforms collider,
 # it can trigger another instant jump, the players negative (upward force) will increase more then normal.
 
+	
+
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-		
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
