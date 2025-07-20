1afbed73337796ec7be00a29765876bd93695452
Wilisimple4u <112163287+Wilisimple4u@users.noreply.github.com>
2025-07-20 21:58:47 +0200

    NoMajorProgress

--- Prof of concept GameJam/OneStepAtTheTime/Scripts/Platform_spawner.gd
+++ Prof of concept GameJam/OneStepAtTheTime/Scripts/Platform_spawner.gd
@@ -1,9 +1,28 @@
-extends StaticBody2D
+extends Node2D
 
-#Ready's the player platform for use and referance inside the script.
-@onready var PlacePlatform = preload("res://OneStepAtTheTime/ObjectScenes/PlayerPlatform.tscn")
 
-func _physics_process(delta: float):
-	if Input.is_action_just_pressed("ui_down"):
-		var PlatformPlaced = PlacePlatform.instantiate()
-		add_child(PlatformPlaced)
+var Platform = preload("res://OneStepAtTheTime/ObjectScenes/playerPlatform.tscn")
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		placePlatform()
+
+func placePlatform():
+	var PP = Platform.instantiate()
+	get_parent().add.child(PP)
+
+
+
+
+
+
+#Storage
+#@onready var RiggedPlayerCharacter = get_tree().get_root().get_node("RiggedPlayerCharacter")
+#@onready var PlayerPlatform = load("res://OneStepAtTheTime/ObjectScenes/playerPlatform.tscn")
+
+#func _ready():
+	#placePlatform()
+
+
+#func placePlatform():
+	#var placedplatform = PlayerPlatform.instantiate()
