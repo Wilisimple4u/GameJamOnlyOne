1c7d1ffcc0a69b6e653ee98a6ccf6db521856267
Amund <amhorg1@gmail.com>
2025-07-22 15:26:08 +0200

    rock spawner small update

---
+++ Prof of concept GameJam/OneStepAtTheTime/ObjectScenes/rock.gd
@@ -0,0 +1,5 @@
+extends RigidBody2D
+
+func _on_area_2d_body_entered(body):
+		print("Rock Delete!")
+		queue_free()
--- Prof of concept GameJam/OneStepAtTheTime/Scripts/rockSpawner.gd
+++ Prof of concept GameJam/OneStepAtTheTime/Scripts/rockSpawner.gd
@@ -1,13 +1,16 @@
 extends Node2D
 @onready var rock_scene = preload("res://OneStepAtTheTime/ObjectScenes/rock.tscn")
-var spawn_area = 0
+var spawn_area_x = 0
+var spawn_area_y = 0
+var rocks_limit = 1000
 
 func _ready():
-	spawn_area = get_viewport().size.x
-
+	spawn_area_x = get_viewport().size.x
+	spawn_area_y = get_viewport().size.y
 
 func _physics_process(_delta):
-	var rock = rock_scene.instantiate()
-	add_child(rock)
-	rock.position = Vector2(randi_range(-spawn_area, spawn_area), -10)
-	
+	var rocks_in_game = get_child_count()
+	if rocks_in_game < rocks_limit :
+		var rock = rock_scene.instantiate()
+		add_child(rock)
+		rock.position = Vector2(randi_range(-spawn_area_x, spawn_area_x), -spawn_area_y)
