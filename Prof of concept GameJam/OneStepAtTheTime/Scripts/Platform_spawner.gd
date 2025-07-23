245fe17874fe2764fa8200f628cd972e38087a27
Wilisimple4u <112163287+Wilisimple4u@users.noreply.github.com>
2025-07-23 20:13:44 +0200

    Commentary

--- Prof of concept GameJam/OneStepAtTheTime/Scripts/Platform_spawner.gd
+++ Prof of concept GameJam/OneStepAtTheTime/Scripts/Platform_spawner.gd
@@ -1,26 +1,27 @@
 extends Node2D
-
+#Preloads the PlayerPlatform for use withing the script.
 const PlayPlatfrom = preload("res://OneStepAtTheTime/ObjectScenes/playerPlatform.tscn")
 
-
+#If the player presses the left mouse button it runs the spawnplatform function.
 func _physics_process(_delta):
 	if Input.is_action_just_pressed("Mouse_LeftClick"):
 		spawnplatform()
 		print("platform placed")
 	
 
-
+#Checks then deletes the previous playerplatform if it exists then creates a new playerplatform in the level scene.
 func spawnplatform():
 	var new_Platform = PlayPlatfrom.instantiate()
-	if get_node("/root/Node2D/Temp"):
-		get_node("/root/Node2D/Temp").queue_free()
-	elif get_node("/root/Node2D/Temp2"):
-		get_node("/root/Node2D/Temp2").queue_free()
+	if has_node("/root/Node2D/PlayerPlatform"):
+		get_node("/root/Node2D/PlayerPlatform").queue_free()
+	elif has_node("/root/Node2D/PlayerPlatform2"):
+		get_node("/root/Node2D/PlayerPlatform2").queue_free()
 	else:
 		pass
 	add_child(new_Platform)
-	new_Platform.name = "Temp"
-	new_Platform.position = get_global_mouse_position() 
+	new_Platform.name = "PlayerPlatform"
+	new_Platform.position = get_global_mouse_position()
+	 
 
 
 
