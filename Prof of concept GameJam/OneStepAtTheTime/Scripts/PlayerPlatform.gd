e83aca216d5b08781a8b7ad1bc184e66eccd0013
Wilisimple4u <112163287+Wilisimple4u@users.noreply.github.com>
2025-07-24 19:20:16 +0200

    PlayerDetection

--- Prof of concept GameJam/OneStepAtTheTime/Scripts/PlayerPlatform.gd
+++ Prof of concept GameJam/OneStepAtTheTime/Scripts/PlayerPlatform.gd
@@ -1,1 +1,12 @@
 extends StaticBody2D
+
+
+func _on_area_2d_body_entered(body):
+	if body is CharacterBody2D:
+		print("CharacterBody2D entered!")
+	
+
+
+func _on_area_2d_body_exited(body):
+	if body is CharacterBody2D:
+		print("CharacterBody2D left!")
