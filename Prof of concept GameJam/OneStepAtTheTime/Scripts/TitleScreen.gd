2062a84fb8a0077f98d8ca5525f4f0f699ab3d2c
Wilisimple4u <112163287+Wilisimple4u@users.noreply.github.com>
2025-07-25 21:58:43 +0200

    TempSettings

--- Prof of concept GameJam/OneStepAtTheTime/Scripts/TitleScreen.gd
+++ Prof of concept GameJam/OneStepAtTheTime/Scripts/TitleScreen.gd
@@ -2,17 +2,27 @@ extends Control
 
 const TestLevel = preload("res://OneStepAtTheTime/GameScenes/TempLevel.tscn")
 
-#Listens and awaits an action from the start menu.
-#When the quit button on the start menu is pressed it get's the quit command, closing the game Obviously.
-func _on_quit_start_button_pressed():
-	get_tree().quit()
 
-#When the NewGameButtong is pressed on the start menu, 
+
+#When the NewGameButton is pressed on the start menu, 
 #It get's the change scene to file command changing it to our selected level scene.
 #Currently it is just the TestLevela
 func _on_new_game_button_pressed():
 	get_tree().change_scene_to_file("res://OneStepAtTheTime/GameScenes/TempLevel.tscn")
 
-
+#When the ContinueButton is pressed on the start menu, 
+#It get's the change scene to file command changing it to our selected level scene.
+#Currently it is just the Template of the mainlevel
 func _on_continue_game_button_pressed():
 	get_tree().change_scene_to_file("res://OneStepAtTheTime/GameScenes/main_world_level.tscn")
+
+#When the SettingsButton is pressed on the start menu, 
+#It get's the change scene to file command changing it to our selected level scene.
+#Currently it is just the Template of the mainlevel
+func _on_settings_start_button_pressed():
+	get_tree().change_scene_to_file("res://settings_menu.tscn")
+
+#Listens and awaits an action from the start menu.
+#When the QuitButton on the start menu is pressed it get's the quit command, closing the game Obviously.
+func _on_quit_start_button_pressed():
+	get_tree().quit()
