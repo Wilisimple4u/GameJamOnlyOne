88b8d8e0106df82479aee20445ca310137402410
Wilisimple4u <112163287+Wilisimple4u@users.noreply.github.com>
2025-07-26 22:33:17 +0200

    WorkingPause

--- Prof of concept GameJam/OneStepAtTheTime/Scripts/WorldLevelScript.gd
+++ Prof of concept GameJam/OneStepAtTheTime/Scripts/WorldLevelScript.gd
@@ -2,7 +2,14 @@ extends Node2D
 
 #Preloads the PlayerPlatform for use withing the script.
 const PlayPlatfrom = preload("res://OneStepAtTheTime/ObjectScenes/playerPlatform.tscn")
+@onready var gameplay_menu = $PanelContainer
 
+func _ready():
+	gameplay_menu.hide()
+	
+func _input(event):
+	if event.is_action_pressed("Pause"):
+		pause_state()
 
 #If the player presses the left mouse button it runs the spawnplatform function.
 func _physics_process(_delta):
@@ -24,3 +31,19 @@ func spawnplatform():
 	add_child(new_Platform)
 	new_Platform.name = "PlayerPlatform"
 	new_Platform.position = get_global_mouse_position()
+
+func pause_state():
+	if not gameplay_menu.visible:
+		gameplay_menu.show()
+		get_tree().paused = true
+	else:
+		gameplay_menu.hide()
+		get_tree().paused = false
+
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://Main menu.tscn")
+
+#Requires to toggle the platform so it can't create a platform on the menu buttons.
+func _on_button_pressed():
+	pause_state()
