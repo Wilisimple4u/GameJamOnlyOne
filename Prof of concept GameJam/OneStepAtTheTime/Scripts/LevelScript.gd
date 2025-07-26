8ad058a37fc0c05aed772c4c40a2ee42639ba58f
Wilisimple4u <112163287+Wilisimple4u@users.noreply.github.com>
2025-07-26 11:13:49 +0200

    WebExport

--- Prof of concept GameJam/OneStepAtTheTime/Scripts/LevelScript.gd
+++ Prof of concept GameJam/OneStepAtTheTime/Scripts/LevelScript.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 #Preloads the PlayerPlatform for use withing the script.
-const PlayPlatfrom = preload("res://OneStepAtTheTime/ObjectScenes/playerPlatform.tscn")
+const PlayPlatform = preload("res://OneStepAtTheTime/ObjectScenes/playerPlatform.tscn")
 
 
 #If the player presses the left mouse button it runs the spawnplatform function.
@@ -14,7 +14,7 @@ func _physics_process(_delta):
 	
 #Checks then deletes the previous playerplatform if it exists then creates a new playerplatform in the level scene.
 func spawnplatform():
-	var new_Platform = PlayPlatfrom.instantiate()
+	var new_Platform = PlayPlatform.instantiate()
 	if has_node("/root/Node2D/PlayerPlatform"):
 		get_node("/root/Node2D/PlayerPlatform").queue_free()
 	elif has_node("/root/Node2D/PlayerPlatform2"):
