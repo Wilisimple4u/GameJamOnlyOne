70a9a8d0c6d3b000f777a73ccdb3c2ba16fd856c
Amund <amhorg1@gmail.com>
2025-07-27 23:10:03 +0200

    small commit

--- Prof of concept GameJam/OneStepAtTheTime/Scripts/Player_char_Move.gd
+++ Prof of concept GameJam/OneStepAtTheTime/Scripts/Player_char_Move.gd
@@ -1,9 +1,5 @@
 extends CharacterBody2D
-@onready var can_be_seen = $VisibleOnScreenNotifier2D
-@onready var hitbox = $CollisionShape2D
-@onready var sprite = $AnimatedSprite2D
 const PlayPlatform = preload("res://OneStepAtTheTime/ObjectScenes/playerPlatform.tscn")
-var platform_limit = 1
 #Importing and reading the platform to be used.
 #@onready var PlayerPlatform = load("res://OneStepAtTheTime/ObjectScenes/playerPlatform.tscn")
 const SPEED = 150.0
@@ -13,14 +9,13 @@ const JUMP_VELOCITY = -300.0
 # it can trigger another instant jump, the players negative (upward force) will increase more then normal.
 
 func _ready() -> void:
-	pass
+	Camera.global_position.y = 0
 		
-func _physics_process(delta):	
+func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-
 	# Handle jump.
 	if Input.is_action_just_pressed("W_and_Space_Key") and is_on_floor():
 		velocity.y = (JUMP_VELOCITY)	
@@ -29,14 +24,6 @@ func _physics_process(delta):
 	if Camera.global_position.y > self.global_position.y:
 		Camera.global_position.y = self.global_position.y
 		
-	#Reset game if player gooes offscreen and dies	
-	#if !can_be_seen.is_on_screen():
-	#	get_tree().reload_current_scene()
-	#	Camera.global_position.y = 0
-	#	#Reload level
-	#elif can_be_seen.is_on_screen():
-	#	pass
- 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("A_Key", "D_Key")
@@ -47,11 +34,8 @@ func _physics_process(delta):
 
 	move_and_slide()
 	
-
-
 	if Input.is_action_just_pressed("Mouse_LeftClick") and get_tree().current_scene.name == "Node2D":
 		spawnplatform()
-		#print("platform placed")
 
 func remove_platform():
 	for node in get_tree().get_nodes_in_group("platforms"):
@@ -59,9 +43,6 @@ func remove_platform():
 
 #Checks then deletes the previous playerplatform if it exists then creates a new playerplatform in the level scene.
 func spawnplatform():
-	#var path1 = "/root/Node2D/TempPlayerChar/PlayerPlatform"
-	#var path2 = "/root/TempPlayerMove/PlayerPlatform"
-	#for nodes in get_tree().get_nodes_in_group("platfrom"):
 	remove_platform()
 	var new_Platform = PlayPlatform.instantiate()
 	new_Platform.top_level = true
@@ -69,3 +50,6 @@ func spawnplatform():
 	new_Platform.name = "PlayerPlatform"
 	new_Platform.position = get_global_mouse_position()
 	
+#Reset game and position if player leaves screen	
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	get_tree().reload_current_scene()
